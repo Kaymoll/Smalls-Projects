56d1b88a8278b1e6431fd41f39e0b0da0c10e01a
Camm <camillelcd7@gmail.com>
2025-10-25 23:37:38 +0200

    refactor: structure fichiers

---
+++ calculatrice/calculatrice.js
@@ -0,0 +1,113 @@
+const operatorButton = document.querySelectorAll(".operator");
+const numberButtons = document.querySelectorAll("[data-number]");
+const display = document.querySelector("#display");
+const delButton = document.querySelector(".clear");
+const equalsButton = document.querySelector(".equals");
+let number1 = "";
+let number2 = "";
+let operator = "";
+
+function showDisplay(element){
+    display.innerHTML = element;
+}
+
+function supp(){
+    number1 = "";
+    number2 = "";
+    operator = "";
+    showDisplay("0");
+}
+
+delButton.addEventListener('click' ,supp)
+
+function operatorSelector (event){
+    const op = event.target.dataset.operator;
+        if (number1 === "") {
+            return;
+        }else if (operator !== "") {
+            return;
+        }else {
+            operator = op;
+            showDisplay(number1 + operator);
+        }
+        
+    };
+
+for(let opButton of operatorButton){
+    opButton.addEventListener('click',operatorSelector);
+}
+
+function numberButton(event) {
+    const value = event.target.dataset.number;
+        if (operator === "") {
+            number1 += value;
+            showDisplay(number1);
+        } else {
+            number2 += value;
+            showDisplay(number1 + operator + number2);
+        }
+}
+for (buttonNumber of numberButtons) {
+    buttonNumber.addEventListener('click', numberButton);
+}
+
+function calcul() {
+    let result = 0;
+    const n1 = Number(number1);
+    const n2 = Number(number2);
+    switch (operator) {
+        case "+":
+            result = n1 + n2;
+            break;
+        case "-":
+            result = n1 - n2;
+            break;
+        case "*":
+            result = n1 * n2;
+            break;
+        case "/":
+            if (n2 === 0) {
+                result = "Erreur";
+            }else {
+                result = n1 / n2;
+            }
+            break;
+        default:
+            return;
+    }
+    showDisplay(String(result));
+}
+
+equalsButton.addEventListener("click",calcul);
+
+function keybord(event){
+    const key = event.key;
+    if (key >= '0' && key <= '9'){
+        numberButtonKeyBoard(key)
+    }
+}
+
+function operatorSelectorkeyboard (op){
+    console.log(op);
+        if (number1 === "") {
+            return;
+        }else if (operator !== "") {
+            return;
+        }else {
+            operator = op;
+            showDisplay(number1 + operator);
+        }
+        
+    };
+
+function numberButtonKeyBoard(op) {
+        if (operator === "") {
+            number1 += op;
+            showDisplay(number1);
+        } else {
+            number2 += op;
+            showDisplay(number1 + operator + number2);
+        }
+}
+
+document.addEventListener('keydown', keybord)
